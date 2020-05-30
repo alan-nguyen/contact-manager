7fb669f5c4f65dbec45dfff3fcb3c59c6bc58520
Alan Nguyen <tuan8101@gmail.com>
2020-05-30 00:13:04 -0600

    Wrap all contacts inside a container

--- src/App.js
+++ src/App.js
@@ -10,12 +10,18 @@ class App extends Component {
     return (
       <div className="App">
         <Header branding="Contact Manager" />
-        <Contact name="John Doe" email="jdoe@gmail.com" phone="555-555-5555" />
-        <Contact
-          name="Karen Smith"
-          email="karen@gmail.com"
-          phone="444-444-4444"
-        />
+        <div className="container">
+          <Contact
+            name="John Doe"
+            email="jdoe@gmail.com"
+            phone="555-555-5555"
+          />
+          <Contact
+            name="Karen Smith"
+            email="karen@gmail.com"
+            phone="444-444-4444"
+          />
+        </div>
       </div>
     );
   }
