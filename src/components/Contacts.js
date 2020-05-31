da0994530f6b361347b3c408181f5049eab2c7b2
Alan Nguyen <tuan8101@gmail.com>
2020-05-31 00:06:29 -0600

    Add key to Contact

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -34,6 +34,7 @@ class Contacts extends Component {
       <div>
         {contacts.map((contact) => (
           <Contact
+            key={contact.id}
             name={contact.name}
             email={contact.email}
             phone={contact.phone}
