11bd8cfd28a8b974a98c02f701b06ce23ae2da41
Alan Nguyen <tuan8101@gmail.com>
2020-05-31 00:01:34 -0600

    Render contacts

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
 
 class Contacts extends Component {
+  constructor() {
+    super();
+    this.state = {
+      contacts: [
+        {
+          id: 1,
+          name: 'John Doe',
+          email: 'jdoe@gmail.com',
+          phone: '555-555-5555',
+        },
+        {
+          id: 2,
+          name: 'Karen Williams',
+          email: 'karen@gmail.com',
+          phone: '333-333-3333',
+        },
+        {
+          id: 3,
+          name: 'Alex Smith',
+          email: 'alex@gmail.com',
+          phone: '444-444-4444',
+        },
+      ],
+    };
+  }
   render() {
+    const { contacts } = this.state;
+
     return (
       <div>
-        <h1></h1>
+        {contacts.map((contact) => (
+          <h1>{contact.name}</h1>
+        ))}
       </div>
     );
   }
