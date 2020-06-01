602ec9d39d5e51d384b516d887305eb5ba66366f
Alan Nguyen <tuan8101@gmail.com>
2020-05-31 23:33:11 -0600

    Create onShowClick method

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,8 +2,13 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class Contact extends Component {
+  state = {};
+
   static propTypes = {
     contact: PropTypes.object.isRequired,
+  };
+  onShowClick = (name, e) => {
+    console.log(name);
   };
 
   render() {
@@ -11,7 +16,11 @@ class Contact extends Component {
     return (
       <div className="card card-body mb-3">
         <h4>
-          {name} <i className="fas fa-sort-down"></i>
+          {name}{' '}
+          <i
+            onClick={this.onShowClick.bind(this, name)}
+            className="fas fa-sort-down"
+          ></i>
         </h4>
         <ul className="list-group">
           <li className="list-group-item">Email: {email}</li>
