10b5cdd166d236d2f7488183a84387fdc09b3a1e
Alan Nguyen <tuan8101@gmail.com>
2020-06-01 23:38:06 -0600

    Create Provider class

--- src/context.js
+++ src/context.js
@@ -1,3 +1,28 @@
 import React, { Component } from 'react';
 
 const Context = React.createContext();
+
+export class Provider extends Component {
+  state = {
+    contacts: [
+      {
+        id: 1,
+        name: 'John Doe',
+        email: 'jdoe@gmail.com',
+        phone: '555-555-5555',
+      },
+      {
+        id: 2,
+        name: 'Karen Williams',
+        email: 'karen@gmail.com',
+        phone: '333-333-3333',
+      },
+      {
+        id: 3,
+        name: 'Alex Smith',
+        email: 'alex@gmail.com',
+        phone: '444-444-4444',
+      },
+    ],
+  };
+}
