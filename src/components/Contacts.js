73858fba790ea5a7f63cccf461b993b1b4fa6098
Alan Nguyen <tuan8101@gmail.com>
2020-06-01 23:37:46 -0600

    Create context.js

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -4,28 +4,6 @@ import Contact from './Contact';
 class Contacts extends Component {
   constructor() {
     super();
-    this.state = {
-      contacts: [
-        {
-          id: 1,
-          name: 'John Doe',
-          email: 'jdoe@gmail.com',
-          phone: '555-555-5555',
-        },
-        {
-          id: 2,
-          name: 'Karen Williams',
-          email: 'karen@gmail.com',
-          phone: '333-333-3333',
-        },
-        {
-          id: 3,
-          name: 'Alex Smith',
-          email: 'alex@gmail.com',
-          phone: '444-444-4444',
-        },
-      ],
-    };
   }
 
   deleteContact = (id) => {
