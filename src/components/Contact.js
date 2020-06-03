5c987c6d88ab01cdb44092ba0c0cdcdb97a449db
Alan Nguyen <tuan8101@gmail.com>
2020-06-02 23:24:39 -0600

    Wrap card into Consumer

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Consumer } from '../context';
 
 class Contact extends Component {
   state = {
@@ -15,30 +16,39 @@ class Contact extends Component {
   render() {
     const { name, email, phone } = this.props.contact;
     const { showContactInfo } = this.state;
+
     return (
-      <div className="card card-body mb-3">
-        <h4>
-          {name}{' '}
-          <i
-            onClick={() => {
-              this.setState({ showContactInfo: !this.state.showContactInfo });
-            }}
-            className="fas fa-sort-down"
-            style={{ cursor: 'pointer' }}
-          ></i>
-          <i
-            className="fas fa-times"
-            style={{ cursor: 'pointer', float: 'right', color: 'red' }}
-            onClick={this.onDeleteClick}
-          ></i>
-        </h4>
-        {showContactInfo ? (
-          <ul className="list-group">
-            <li className="list-group-item">Email: {email}</li>
-            <li className="list-group-item">Phone: {phone}</li>
-          </ul>
-        ) : null}
-      </div>
+      <Consumer>
+        {(value) => {
+          return (
+            <div className="card card-body mb-3">
+              <h4>
+                {name}{' '}
+                <i
+                  onClick={() => {
+                    this.setState({
+                      showContactInfo: !this.state.showContactInfo,
+                    });
+                  }}
+                  className="fas fa-sort-down"
+                  style={{ cursor: 'pointer' }}
+                ></i>
+                <i
+                  className="fas fa-times"
+                  style={{ cursor: 'pointer', float: 'right', color: 'red' }}
+                  onClick={this.onDeleteClick}
+                ></i>
+              </h4>
+              {showContactInfo ? (
+                <ul className="list-group">
+                  <li className="list-group-item">Email: {email}</li>
+                  <li className="list-group-item">Phone: {phone}</li>
+                </ul>
+              ) : null}
+            </div>
+          );
+        }}
+      </Consumer>
     );
   }
 }
