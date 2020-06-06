fba52e49835af46af8f1cd31087de6862e191741
Alan Nguyen <tuan8101@gmail.com>
2020-06-06 00:10:30 -0600

    Install and import classnames

--- src/components/layout/TextInputGroup.js
+++ src/components/layout/TextInputGroup.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 
 const TextInputGroup = ({
   label,
@@ -8,6 +9,7 @@ const TextInputGroup = ({
   placeholder,
   type,
   onChange,
+  error,
 }) => {
   return (
     <div className="form-group">
