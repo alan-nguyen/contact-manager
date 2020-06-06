f3aeb0054e1c5a143ff04be2dba0bdb433922629
Alan Nguyen <tuan8101@gmail.com>
2020-06-05 23:47:42 -0600

    Create TextInputGroup

---
+++ src/components/layout/TextInputGroup.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const TextInputGroup = ({
+  label,
+  name,
+  value,
+  placeholder,
+  type,
+  onChange,
+}) => {
+  return (
+    <div className="form-group">
+      <label htmlFor={name}>{label}</label>
+      <input
+        type={type}
+        name={name}
+        className="form-control form-control-lg"
+        placeholder={placeholder}
+        value={value}
+        onChange={this.onChange}
+      />
+    </div>
+  );
+};
+
+export default TextInputGroup;
