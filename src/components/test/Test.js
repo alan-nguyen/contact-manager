2e00c0290e4a62f2e0dd5cbfcb9407f92e8a4ea8
Alan Nguyen <tuan8101@gmail.com>
2020-06-10 22:51:08 -0600

    Test DidMount, WillMount, DidUpdate

--- src/components/test/Test.js
+++ src/components/test/Test.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
 
 class Test extends Component {
+  componentDidMount() {
+    console.log('componentDidMount...');
+  }
+  componentWillMount() {
+    console.log('componentWillMount...');
+  }
+
+  componentDidUpdate() {
+    console.log('componentDidUpdate...');
+  }
+
   render() {
     return (
       <div>
