2998208f056445162bc3701c2e744fdcd2b12527
Alan Nguyen <tuan8101@gmail.com>
2020-06-10 22:59:55 -0600

    Add GET request

--- src/components/test/Test.js
+++ src/components/test/Test.js
@@ -2,17 +2,18 @@ import React, { Component } from 'react';
 
 class Test extends Component {
   state = {
-    test: 'test',
+    title: '',
+    body: '',
   };
   componentDidMount() {
-    console.log('componentDidMount...');
-  }
-  componentWillMount() {
-    console.log('componentWillMount...');
-  }
-
-  componentDidUpdate() {
-    console.log('componentDidUpdate...');
+    fetch('https://jsonplaceholder.typicode.com/posts/1')
+      .then((response) => response.json())
+      .then((data) =>
+        this.setState({
+          title: data.title,
+          body: data.body,
+        })
+      );
   }
 
   render() {
