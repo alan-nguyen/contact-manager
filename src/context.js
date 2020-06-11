76aa1b312989099cc924cab3d8aa87cb5fa9dc39
Alan Nguyen <tuan8101@gmail.com>
2020-06-10 23:03:23 -0600

    Remove hardcode contacts

--- src/context.js
+++ src/context.js
@@ -23,26 +23,7 @@ const reducer = (state, action) => {
 
 export class Provider extends Component {
   state = {
-    contacts: [
-      {
-        id: 1,
-        name: 'John Doe',
-        email: 'jdoe@gmail.com',
-        phone: '555-555-5555',
-      },
-      {
-        id: 2,
-        name: 'Karen Williams',
-        email: 'karen@gmail.com',
-        phone: '333-333-3333',
-      },
-      {
-        id: 3,
-        name: 'Alex Smith',
-        email: 'alex@gmail.com',
-        phone: '444-444-4444',
-      },
-    ],
+    contacts: [],
     dispatch: (action) => {
       this.setState((state) => reducer(state, action));
     },
