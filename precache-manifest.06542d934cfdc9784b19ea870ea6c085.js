24ffc82c27ab68b985a2fa1ed23c06392bb1c564
Alan Nguyen <tuan8101@gmail.com>
2020-07-22 00:01:56 -0600

    Updates

---
+++ precache-manifest.06542d934cfdc9784b19ea870ea6c085.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2c564aa79491762a025cd1081d07e90",
+    "url": "/contact-manager/index.html"
+  },
+  {
+    "revision": "35507b464bc2fd9c3b00",
+    "url": "/contact-manager/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ee016814859c72ca3127",
+    "url": "/contact-manager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "35507b464bc2fd9c3b00",
+    "url": "/contact-manager/static/js/2.2a38c816.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contact-manager/static/js/2.2a38c816.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee016814859c72ca3127",
+    "url": "/contact-manager/static/js/main.60ba0986.chunk.js"
+  },
+  {
+    "revision": "4cdc90b35cd2e7a7a661",
+    "url": "/contact-manager/static/js/runtime-main.7f261af7.js"
+  }
+]);
