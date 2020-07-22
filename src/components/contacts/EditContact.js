c2ccc35956699267ca86defcd97eca133cedbdb0
Alan Nguyen <tuan8101@gmail.com>
2020-07-21 22:11:07 -0600

    Add link

--- src/components/contacts/EditContact.js
+++ src/components/contacts/EditContact.js
@@ -52,7 +52,7 @@ class EditContact extends Component {
           const { dispatch } = value;
           return (
             <div className="card mb-3">
-              <div className="card-header">Add Contact</div>
+              <div className="card-header">Edit Contact</div>
               <div className="card-body">
                 <form onSubmit={this.onSubmit.bind(this, dispatch)}>
                   <TextInputGroup
